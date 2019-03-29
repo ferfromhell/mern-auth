51b063d8b40be7c9c1c1c3567d662770d257f0de
Fernando Lujan <fernando@Fernandos-MacBook-Pro.local>
2019-03-29 10:15:09 -0700

    remove cors

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ const users = require('./routes/api/users');
 const app = express();
 
 //CORS
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 //Body parser
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
